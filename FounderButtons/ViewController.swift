9129845820c8e885098c76ff438e27cd98db0153
Küng Hansueli <system-shock@web.de>
2024-11-19 08:58:42 +0100

    Section 2.3 completed

--- FounderButtons/ViewController.swift
+++ FounderButtons/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("👍🏼 viewDidLoad has run👍🏼!")
     }
-
+    @IBAction func googleFoundersButton(_ sender: UIButton) {
+        print("googleFoundersButton pressed🤪!")
+        firstLabel.text = "Lary Page"
+        secondLabel.text = "Sergey Brin"
+    }
+    @IBAction func runwayFoundersButton(_ sender: UIButton) {
+        print("runwayFoundersButton pressed😍!")
+        firstLabel.text = "Jennifer Hyman"
+        secondLabel.text = "Jenny Fleiss"
+    }
+    @IBAction func clearButton(_ sender: UIButton) {
+        print("clearButton pressed😄!")
+        firstLabel.text = ""
+        secondLabel.text = ""
+    }
+    
 
 }
 
