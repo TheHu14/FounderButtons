98b5aada568e28036c605e58df9ef54dfce862f8
Küng Hansueli <system-shock@web.de>
2024-11-19 08:31:43 +0100

    Initial Commit

---
+++ FounderButtonsTests/FounderButtonsTests.swift
@@ -0,0 +1,17 @@
+//
+//  FounderButtonsTests.swift
+//  FounderButtonsTests
+//
+//  Created by Küng Hansueli on 19.11.2024.
+//
+
+import Testing
+@testable import FounderButtons
+
+struct FounderButtonsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ FounderButtonsUITests/FounderButtonsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  FounderButtonsUITestsLaunchTests.swift
+//  FounderButtonsUITests
+//
+//  Created by Küng Hansueli on 19.11.2024.
+//
+
+import XCTest
+
+final class FounderButtonsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
